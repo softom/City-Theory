e0dae0dec0596c4b2ecc016d31057e0b2b892cfb
Artem Y. Antipov <artem.antipov@gmail.com>
2023-03-15 20:08:15 +0300

    First commit, City project and .gitignore

---
+++ Assets/Camera_Rig_Script.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Rig_Script : MonoBehaviour
+{
+    public float moveTime;
+    public float moveSpeed;
+    public Vector3 newPosition;
+    public Vector3 cameraPosition;
+    public Camera cameraToWork;
+    private Transform cameraTransform;
+    
+    public float rotationSpeed;
+    public Quaternion rotationCamera;
+    public Vector3 zoomAmmount;
+    public float zoomMultiplay;
+
+    public Vector3 currentMouseClick;
+    public Vector3 clickMousePossition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        newPosition = transform.position;
+        rotationCamera = transform.rotation;
+        
+        cameraTransform = cameraToWork.transform;
+        cameraPosition = cameraTransform.localPosition;
+        
+        zoomAmmount = cameraTransform.TransformDirection(Vector3.forward).normalized;
+        zoomAmmount *= zoomMultiplay;
+        Debug.DrawRay(cameraTransform.position, zoomAmmount,Color.green);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInputs();
+        HandleMouse();
+    }
+
+    public void HandleMouse()
+    {
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Plane mousePlane = new Plane(Vector3.up, Vector3.zero);
+            Debug.DrawRay(Vector3.zero, Vector3.up, Color.yellow, 30);
+            Debug.DrawRay(Vector3.zero, Vector3.right, Color.green, 30);
+            Debug.DrawRay(Vector3.zero, Vector3.forward, Color.green, 30);
+            Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawLine(cameraRay.origin, cameraRay.direction, Color.red, 30f);
+            Debug.DrawRay(cameraRay.origin, cameraRay.direction, Color.blue, 30f);
+            //Debug.
+
+
+            float entery;
+
+            if (mousePlane.Raycast(cameraRay, out entery))
+            {
+                clickMousePossition = cameraRay.GetPoint(entery);
+            }
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+                Plane mousePlane = new Plane(Vector3.up, Vector3.zero);
+                
+                Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+                
+                float entery;
+                if (mousePlane.Raycast(cameraRay, out entery))
+                {
+                    currentMouseClick = cameraRay.GetPoint(entery);
+                    newPosition = transform.position + clickMousePossition - currentMouseClick;
+
+                }
+        }
+    }
+
+
+    public void HandleInputs()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            newPosition += (transform.forward * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            newPosition -= (transform.forward * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            newPosition -= (transform.right * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            newPosition += (transform.right * moveSpeed);
+        }
+
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * moveTime);
+        
+        //Вращение камерой
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rotationCamera *= Quaternion.Euler(Vector3.up * rotationSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            rotationCamera *= Quaternion.Euler(Vector3.down * rotationSpeed);
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotationCamera, Time.deltaTime * moveTime);
+
+        //Пиблиэение и удаление
+        if (Input.GetKey(KeyCode.R))
+        {
+            cameraPosition += zoomAmmount;
+        }
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            cameraPosition -= zoomAmmount;
+        }
+
+        cameraToWork.transform.localPosition =
+            Vector3.Lerp(cameraToWork.transform.localPosition, cameraPosition, Time.deltaTime * moveTime);
+        
+
+        //if (Physics.Raycast(t.position, look, out $$anonymous$$t, weaponlength, layerMask)) 
+    }
+}
